fb44e77d8c4a522acf33315eedd13cc5c82a8bd9
Richard Brown <rabrown1975@gmail.com>
2020-09-09 01:36:26 -0500

    Clears whitelist for dev.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.web_console.whitelisted_ips = '10.0.0.0/16'
+  config.hosts.clear
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
